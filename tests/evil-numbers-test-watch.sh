061de6f89579861e8d0af3dd16fdb78a00871351
Campbell Barton <ideasman42@gmail.com>
2021-08-09 13:20:14 +1000

    Tests: watch evil-numbers.el as well as the tests
    Also add explanation for why initialize in packages is needed.

--- tests/evil-numbers-test-watch.sh
+++ tests/evil-numbers-test-watch.sh
@@ -2,7 +2,7 @@
 SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")"
 echo $SCRIPT_PATH
 while true; do
-    inotifywait -e close_write "$SCRIPT_PATH/evil-numbers-test.el"
+    inotifywait -e close_write "$SCRIPT_PATH/evil-numbers-test.el" "$SCRIPT_PATH/../evil-numbers.el"
     tput clear
     "$SCRIPT_PATH/evil-numbers-test.sh"
 done
--- tests/evil-numbers-test.sh
+++ tests/evil-numbers-test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 SCRIPT_PATH="$(dirname "${BASH_SOURCE[0]}")"
+
+# `package-initialize` is needed so the 'evil' package can be found.
 emacs \
     -batch \
     --eval "(package-initialize)" \
